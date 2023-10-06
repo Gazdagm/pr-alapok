6f323ae0222e7bb7cc54c5761b5cf27c018b48b2
Gazdagm <113088395+Gazdagm@users.noreply.github.com>
2023-10-06 10:07:32 +0200

    feladat_004.py

--- Házi Feladatok/feladat_004.py
+++ Házi Feladatok/feladat_004.py
@@ -1,1 +1,18 @@
-404: Not Found
+#feladat_004
+#jegyek beolvasása
+
+jegy = input("Kérem a jegyed")
+jegy = int(jegy)
+
+if jegy == 1:
+    print(f"Elégtelen")
+elif jegy == 2:
+    print(f"Elégséges")
+elif jegy == 3:
+    print(f"közepes")
+elif jegy == 4:
+    print(f"jó")
+elif jegy == 5:
+    print(f"Kitűnő")
+else:
+    print(f"A jegyet nem ismerte fel a program")
